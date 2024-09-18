253fcf5bd18c271735008ba87ef6282ca4584d47
Alfred Hromas <alfred.hromas@gmail.com>
2024-09-18 21:21:32 +0200

    Additional testing endpoints

---
+++ DocumentManagementSystem/Persistance/Models/Entities/Document.cs
@@ -0,0 +1,10 @@
+﻿namespace DocumentManagementSystem.Persistence.Models.Entities
+{
+	public class Document
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public long Size { get; set; }
+		public DateTime UploadedAt { get; set; }
+	}
+}
--- DocumentManagementSystem/Web/Controllers/DocumentsController.cs
+++ DocumentManagementSystem/Web/Controllers/DocumentsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Asp.Versioning;
+using DocumentManagementSystem.Persistence.Models.Entities;
 
 namespace DocumentManagementSystem.Controllers
 {
@@ -10,16 +11,72 @@ namespace DocumentManagementSystem.Controllers
 	{
 		private readonly ILogger<DocumentsController> _logger;
 
+		// Test data, to be replaced with the actual service
+		private static readonly List<Document> _documents = new List<Document>();
+		private static int nextId = 1;
+
 		public DocumentsController(ILogger<DocumentsController> logger)
 		{
 			_logger = logger;
 		}
 
+		// Upload document
 		[HttpPost(Name = "UploadDocument")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		public async Task<ActionResult> UploadDocumentAsync(IFormFile file)
 		{
-			return Ok();
+			// Testing
+			var document = new Document
+			{
+				Id = nextId++,
+				Name = file.FileName,
+				Size = file.Length,
+				UploadedAt = DateTime.UtcNow
+			};
+
+			_documents.Add(document);
+
+			return Ok(document);
+		}
+
+		// Get document by Id
+		[HttpGet("{id}", Name = "GetDocumentById")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult<Document>> GetDocumentById(int id)
+		{
+			// Testing
+			var document = _documents.FirstOrDefault(d => d.Id == id);
+			if (document == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(document);
+		}
+
+		// Lists all documents
+		[HttpGet(Name = "ListDocuments")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		public async Task<ActionResult> ListDocumentsAsync()
+		{
+			return Ok(_documents);
+		}
+
+		// Delete a specific document based on id
+		[HttpDelete("{id}", Name = "DeleteDocument")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<ActionResult> DeleteDocumentAsync(int id)
+		{
+			var document = _documents.FirstOrDefault(d => d.Id == id);
+			if (document == null)
+			{
+				return NotFound();
+			}
+
+			_documents.Remove(document);
+			return Ok($"Document with ID {id} has been deleted.");
 		}
 	}
 }
