3c7813cb70f44f41aba3e4b78eba344b6550965e
Karolina Aigner <karolinaaigner@gmail.com>
2024-10-20 11:13:43 +0200

    Document Service

---
+++ DocumentManagementSystem/Business/Services/IDocumentService.cs
@@ -0,0 +1,10 @@
+﻿using Business.Models.Domain;
+
+namespace Business.Services;
+public interface IDocumentService
+{
+	Task AddDocument(Document document);
+	Task<Document> GetDocumentById(uint id);
+	Task DeleteDocumentById(uint id);
+	Task<List<Document>> GetAllDocuments();
+}
